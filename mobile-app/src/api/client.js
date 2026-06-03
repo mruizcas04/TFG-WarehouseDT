503f280538ce987b285b0b537ffc33e62326423c
mruizcas04 <mruizcas04@gmail.com>
2026-06-04 01:00:55 +0200

    build app

--- mobile-app/src/api/client.js
+++ mobile-app/src/api/client.js
@@ -1,5 +1,5 @@
 // Cambia esta IP por la de tu PC en la red local (ejecuta `ipconfig` en Windows)
-export const API_URL = 'http://192.168.1.48:8000';
+export const API_URL = 'https://tfg-production-1c10.up.railway.app';
 
 export async function apiFetch(path, options = {}, token = null) {
   const headers = { 'Content-Type': 'application/json', ...options.headers };
